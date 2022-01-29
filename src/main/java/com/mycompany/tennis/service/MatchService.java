fbf4e1ad423229e25776880bb64aa4eb69cf5d4e
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2022-01-29 09:20:52 +0000

    Solution avec DAO qui joue à peu près le même rôle que le repository

--- src/main/java/com/mycompany/tennis/service/MatchService.java
+++ src/main/java/com/mycompany/tennis/service/MatchService.java
@@ -1,21 +1,25 @@
 package com.mycompany.tennis.service;
 
+import com.mycompany.tennis.dao.MatchDaoImpl;
 import com.mycompany.tennis.entity.Match;
 import com.mycompany.tennis.repository.MatchRepositoryImpl;
 import com.mycompany.tennis.repository.ScoreRepositoryImpl;
 
 public class MatchService {
 
-    private MatchRepositoryImpl matchRepository;
-    private ScoreRepositoryImpl scoreRepository;
+    /*private MatchRepositoryImpl matchRepository;
+    private ScoreRepositoryImpl scoreRepository;*/
+    private MatchDaoImpl matchDao;
 
     public MatchService () {
-        this.matchRepository = new MatchRepositoryImpl();
-        this.scoreRepository = new ScoreRepositoryImpl();
+        /*this.matchRepository = new MatchRepositoryImpl();
+        this.scoreRepository = new ScoreRepositoryImpl();*/
+        matchDao = new MatchDaoImpl();
     }
 
     public void saveNewMatch(Match match) {
-        matchRepository.create(match);
-        scoreRepository.create(match.getScore());
+        matchDao.createMatchwithscore(match);
+        /*matchRepository.create(match);
+        scoreRepository.create(match.getScore());*/
     }
 }
